127f875aa5551ab3b8797279ba88750693d3e331
xufj <2022878615@qq.com>
2019-10-28 14:28:51 +0800

    particle

---
+++ particle/UserGUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour {
+
+    public flare flare1, flare2, flare3;
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+       
+    }
+
+    void OnGUI() {
+        if (GUI.Button(new Rect(10, 30, 50, 30), "东风")) {
+            flare1.increaseWindPower();
+            flare2.increaseWindPower();
+            flare3.increaseWindPower();
+        }
+
+        if (GUI.Button(new Rect(10, 70, 50, 30), "西风")) {
+            flare1.decreaseWindPower();
+            flare2.decreaseWindPower();
+            flare3.decreaseWindPower();
+        }
+
+        if (GUI.Button(new Rect(10, 110, 50, 30), "增强")) {
+            flare1.increaseFlame();
+            flare2.increaseFlame();
+            flare3.increaseFlame();
+        }
+        
+        if (GUI.Button(new Rect(10, 150, 50, 30), "减弱")) {
+            flare1.decreaseFlame();
+            flare2.decreaseFlame();
+            flare3.decreaseFlame();
+        }
+    }
+}
---
+++ particle/flare.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flare : MonoBehaviour {
+    ParticleSystem particleSystem;
+    ParticleSystem.ForceOverLifetimeModule forceMode;
+    ParticleSystem.ColorOverLifetimeModule colorMode;
+    int windPower;
+
+    // Use this for initialization
+    void Start() {
+        particleSystem = GetComponent<ParticleSystem>();
+        forceMode = particleSystem.forceOverLifetime;
+        colorMode = particleSystem.colorOverLifetime;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (windPower >= 10 || windPower <= -10) {
+            putOut();
+        }
+    }
+
+    void putOut() {
+        ParticleSystem.MainModule main = particleSystem.main;
+        main.loop = false;
+        main.startLifetime = 2f;
+        main.startSize = 2f;
+        main.startSpeed = 2f;
+        forceMode.x = 0;
+
+        Gradient grad = new Gradient();
+        grad.SetKeys(new GradientColorKey[] { new GradientColorKey(Color.black, 2.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 2.0f) });
+        colorMode.color = grad;
+    }
+
+    public void increaseFlame() {
+        particleSystem.startSize = particleSystem.startSize * 1.1f;
+        particleSystem.startLifetime = particleSystem.startLifetime * 1.1f;
+    }
+
+    public void decreaseFlame() {
+        particleSystem.startSize = particleSystem.startSize * 0.9f;
+        particleSystem.startLifetime = particleSystem.startLifetime * 0.9f;
+    }
+
+    public void increaseWindPower() {
+        windPower++;
+        ParticleSystem.MinMaxCurve temp = forceMode.x;
+        temp.constantMax += -1.2f;
+        forceMode.x = temp;
+    }
+
+    public void decreaseWindPower() {
+        windPower--;
+        ParticleSystem.MinMaxCurve temp = forceMode.x;
+        temp.constantMax += 1.2f;
+        forceMode.x = temp;
+    }
+}
